98b62c4bb6059cb19e617807704ded2681031389
Song Yang <songyang@thoughtworks.com>
2015-04-08 20:53:49 +0800

    video | add youku demo video

--- sites/all/themes/december/templates/node--video-product.tpl.php
+++ sites/all/themes/december/templates/node--video-product.tpl.php
@@ -12,9 +12,9 @@
   <div class="product__main">
     <?php echo $node->field_main_text['und']['0']['value']; ?>
   </div>
-  <ul class="product__video">
-    video
-  </ul>
+  <div class="product__video">
+      <embed src="http://player.youku.com/player.php/sid/XOTIzNDA2OTg4/v.swf" allowFullScreen="true" quality="high" width="480" height="400" align="middle" allowScriptAccess="always" type="application/x-shockwave-flash"></embed>
+  </div>
 </div>
 
 <?php include_once("partials/similar-product.tpl.inc"); ?>
