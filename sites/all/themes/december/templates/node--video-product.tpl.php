76bc8aeb253c7a1fd1ca5dbb5cc2edb80c565121
Song Yang <songyang@thoughtworks.com>
2015-04-13 20:45:18 +0800

    video | use template to render

--- sites/all/themes/december/templates/node--video-product.tpl.php
+++ sites/all/themes/december/templates/node--video-product.tpl.php
@@ -13,7 +13,7 @@
     <?php echo $node->field_main_text['und']['0']['value']; ?>
   </div>
   <div class="product__video">
-      <embed src="http://player.youku.com/player.php/sid/XOTIzNDA2OTg4/v.swf" allowFullScreen="true" quality="high" width="480" height="400" align="middle" allowScriptAccess="always" type="application/x-shockwave-flash"></embed>
+    <?php echo $node->field_video['und']['0']['value']; ?>
   </div>
 </div>
 
