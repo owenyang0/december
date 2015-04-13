491fd0c8e852b9aed49b9bcfa02ff5ee0ef9b2c9
Song Yang <songyang@thoughtworks.com>
2015-04-14 00:48:29 +0800

    react | add produects views

---
+++ sites/all/themes/december/templates/node--standard-page.tpl.php
@@ -0,0 +1,7 @@
+<header class="product__header">
+    <?php include_once("partials/site-nav.tpl.inc"); ?>
+</header>
+<div class="standard__content">
+</div>
+
+<?php include_once("partials/footer.tpl.inc"); ?>
