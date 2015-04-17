11cd8508a105cdcedcffe95fde07129bff08397e
Song Yang <songyang@thoughtworks.com>
2015-04-17 20:21:20 +0800

    sort | add products sort and style

--- sites/all/themes/december/templates/node--standard-page.tpl.php
+++ sites/all/themes/december/templates/node--standard-page.tpl.php
@@ -1,6 +1,12 @@
 <header class="product__header">
     <?php include_once("partials/site-nav.tpl.inc"); ?>
 </header>
+
+<?php if (user_is_logged_in()) {
+  echo $node->body['und']['0']['value'];
+}
+?>
+
 <div class="product__detail"></div>
 
 <?php include_once("partials/footer.tpl.inc"); ?>
