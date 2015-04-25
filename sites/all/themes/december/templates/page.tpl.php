e6b0ee946db1c991a78c9dc346154e5a2743323c
Song Yang <songyang@thoughtworks.com>
2015-04-25 19:44:37 +0800

    template | add page template

--- sites/all/themes/december/templates/page.tpl.php
+++ sites/all/themes/december/templates/page.tpl.php
@@ -1,9 +1,18 @@
 <div class="december">
+
+  <?php if ($page['head']): ?>
+    <div class="december__head">
+      <?php print render($page['head']); ?>
+    </div>
+  <?php endif; ?>
+
   <div class="december__content">
-    <?php echo render($page['content']); ?>
+    <?php print render($page['content']); ?>
   </div>
 
-  <div class="december__footer">
-    <?php include_once("partials/footer.tpl.inc"); ?>
-  </div>
+  <?php if ($page['footer']): ?>
+    <div class="december__footer">
+      <?php include_once("partials/footer.tpl.inc"); ?>
+    </div>
+  <?php endif; ?>
 </div>
