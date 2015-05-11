83f52cb7e0e6b4a6bf4beb5a89fc1aede6c12b2c
Owen Yang <usay@outlook.com>
2015-05-11 23:27:52 +0800

    loading | add circle loading for homepage

--- sites/all/themes/december/template.php
+++ sites/all/themes/december/template.php
@@ -3,6 +3,10 @@
 function december_preprocess_page(&$vars){
   if (isset($vars['node']) && isset($vars['node']->type)) {
     switch($vars['node']->type) {
+      case 'homepage':
+        drupal_add_js(drupal_get_path('theme', 'december') . '/js/progressbar.js');
+        drupal_add_js(drupal_get_path('theme', 'december') . '/js/pace.js');
+        break;
       case 'standard_page':
         drupal_add_js(drupal_get_path('theme', 'december') . '/js/products.js');
         break;
